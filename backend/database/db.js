72063407ffb4619248486b383f9d4df3ec71884a
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-03-28 00:42:52 +0530

    backend completed

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,10 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const DBConnection = async () => {
-    // const USERNAME = process.env.DB_USERNAME;
-    // const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI ="mongodb+srv://tauqueeralam42:YTnoGNvLpCXmMIrx@cluster0.idg9twm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const MONGO_URI = process.env.URI;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
