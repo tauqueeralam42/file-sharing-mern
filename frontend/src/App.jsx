e081d6efca40b809f018959336fec0c209bfe15d
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-03-27 23:44:05 +0530

    api added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import './App.css';
-import { uploadFile } from './service/api';
+import { uploadFile } from './api';
 
 function App() {
   const [file, setFile] = useState('');
