32d5bcdc9414f6380c9c8056723efa657cb2baf4
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-07-29 21:57:31 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const fileInputRef = useRef();
 
-  const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
+  
 
   useEffect(() => {
     const getImage = async () => {
@@ -34,24 +34,26 @@ function App() {
   }
 
   return (
-    <div className='container'>
-      <img src={url} className='img' />
-      <div className='wrapper'>
-        <h1>Simple file sharing!</h1>
+    <div className="container">
+      <div className="wrapper">
+        <h1>Simple File Sharing!</h1>
         <p>Upload and share the download link.</p>
         
-        <button onClick={() => onUploadClick()}>Upload</button>
+        <button className="upload-btn" onClick={() => onUploadClick()}>Upload</button>
         <input
           type="file"
           ref={fileInputRef}
-          style={{ display: "none" }}
+          className="file-input"
           onChange={(e) => setFile(e.target.files[0])}
         />
-
-        <a href={result} target='_blank'>{result}</a> 
+  
+        <a href={result} target="_blank" rel="noopener noreferrer" className="download-link">
+          {result}
+        </a> 
       </div>
     </div>
   );
+  
 }
 
 export default App;
